6b08de7610716350144f0642391ef8201be1cca7
Kyler Sakumoto <kylerws@uw.edu>
2018-12-21 00:23:09 -0800

    Added react router framework

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch /*, Link*/ } from 'react-router-dom'
 
-class App extends Component {}
+import Home from './Home';
+import Nav from './Nav';
+
+class App extends Component {
+  render() {
+    return(
+      <Router basename="/brewski-app">
+        <div>
+          <Nav />
+          <Switch>
+            <Route path="/" component={Home} />
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return(
+      <div>HOMEPAGE</div>
+    );
+  }
+}
+
+export default Home;
